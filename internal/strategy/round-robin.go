54d5d7b294dbaa4b237def634799c014a0c46ce6
Saman <azadi@gmail.com>
2023-08-14 19:00:41 +0330

    Add round robin strategy

---
+++ internal/strategy/round-robin.go
@@ -0,0 +1,46 @@
+package strategy
+
+import (
+	"github.com/samanazadi/load-balancer/internal/node"
+	"net/http"
+	"sync"
+)
+
+type RoundRobin struct {
+	lastUsedIndex int
+	mux           sync.RWMutex // for protecting lastUsedIndex from multiple access
+	Nodes         []*node.Node
+}
+
+func (rr *RoundRobin) nextIndex() int {
+	rr.mux.Lock()
+	defer rr.mux.Unlock()
+	rr.lastUsedIndex++
+	rr.lastUsedIndex = rr.lastUsedIndex % len(rr.Nodes)
+	return rr.lastUsedIndex
+}
+
+func (rr *RoundRobin) GetNextEligibleNode(*http.Request) *node.Node {
+	next := rr.nextIndex()
+	last := next + len(rr.Nodes)
+	for i := next; i < last; i++ {
+		index := i % len(rr.Nodes)
+		if rr.Nodes[index].IsAlive() {
+			if i != next {
+				// store new current index (some unavailable nodes found)
+				rr.mux.Lock()
+				rr.lastUsedIndex = index
+				rr.mux.Unlock()
+			}
+			return rr.Nodes[index]
+		}
+	}
+	return nil // no available node
+}
+
+func NewRoundRobin(nodes []*node.Node) Strategy {
+	return &RoundRobin{
+		lastUsedIndex: -1,
+		Nodes:         nodes,
+	}
+}
