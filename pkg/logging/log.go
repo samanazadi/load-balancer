6ab3c939daf76fa699dd82d89bae8b47f74b5b06
Saman <azadi@gmail.com>
2023-08-16 02:17:45 +0330

    Logging interface
    This commit replace Logger instance with a Log interface.

--- pkg/logging/log.go
+++ pkg/logging/log.go
@@ -2,7 +2,19 @@ package logging
 
 import "log"
 
-var Logger *log.Logger
+type Log interface {
+	Print(v ...any)
+	Printf(format string, v ...any)
+	Println(v ...any)
+	Fatal(v ...any)
+	Fatalf(format string, v ...any)
+	Fatalln(v ...any)
+	Panic(v ...any)
+	Panicf(format string, v ...any)
+	Panicln(v ...any)
+}
+
+var Logger Log
 
 func Init() {
 	Logger = log.Default() // a customized logger can be used
