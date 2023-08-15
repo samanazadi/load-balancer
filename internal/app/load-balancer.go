c9d882aa503f1b63ef8357a921007a0dedc00dc6
Saman <azadi@gmail.com>
2023-08-15 15:28:03 +0330

    Extract node creation logic into node package

--- internal/app/load-balancer.go
+++ internal/app/load-balancer.go
@@ -1,24 +1,23 @@
 package app
 
 import (
-	"context"
 	"github.com/samanazadi/load-balancer/configs"
 	"github.com/samanazadi/load-balancer/internal/algorithm"
 	"github.com/samanazadi/load-balancer/internal/checker"
 	"github.com/samanazadi/load-balancer/internal/models/node"
 	"github.com/samanazadi/load-balancer/pkg/logging"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
-	"time"
 )
-
-const RetryCount = iota
 
 // LoadBalancer is a server pool along an algorithm
 type LoadBalancer struct {
 	serverPool ServerPool
 	algorithm  *algorithm.Algorithm
+}
+
+func (lb *LoadBalancer) SetNodeAlive(url *url.URL, alive bool) {
+	lb.serverPool.setNodeAlive(url, alive)
 }
 
 // ServeHTTP route request based on algorithm
@@ -46,31 +45,7 @@ func New(cfg *configs.Config, chk *checker.ConnectionChecker, alg *algorithm.Alg
 			logging.Logger.Printf("cannot parse node URL: %s", nodeURLString)
 			continue
 		}
-
-		rp := httputil.NewSingleHostReverseProxy(nodeURL)
-		rp.ErrorHandler = func(rw http.ResponseWriter, r *http.Request, e error) { // Active health check
-			retries := getRetryCountFromContext(r)
-			logging.Logger.Printf("active health check, node down, %d retires: %s (%s)",
-				retries, nodeURL, e.Error())
-			if retries < cfg.HealthCheck.Active.MaxRetry {
-				// same node, more retries after some delay
-				retryDelay := time.Millisecond * time.Duration(cfg.HealthCheck.Active.RetryDelay)
-				select {
-				case <-time.After(retryDelay):
-					ctx := context.WithValue(r.Context(), RetryCount, retries+1)
-					rp.ServeHTTP(rw, r.WithContext(ctx))
-				}
-				return
-			}
-
-			// max retries exceeded
-			logging.Logger.Printf("active health check, node down, retires exceeded: %s", nodeURL)
-			lb.serverPool.setNodeAlive(nodeURL, false)
-			newCtx := context.WithValue(r.Context(), RetryCount, 1)
-			lb.ServeHTTP(rw, r.WithContext(newCtx))
-		}
-
-		nodes = append(nodes, node.New(nodeURL, rp, true))
+		nodes = append(nodes, node.New(nodeURL, true, cfg, lb))
 		logging.Logger.Printf("node added: %s", nodeURLString)
 	}
 
@@ -81,11 +56,4 @@ func New(cfg *configs.Config, chk *checker.ConnectionChecker, alg *algorithm.Alg
 	lb.StartPassiveHealthCheck(cfg.HealthCheck.Passive.Period)
 
 	return lb
-}
-
-func getRetryCountFromContext(r *http.Request) int {
-	if retryCount, ok := r.Context().Value(RetryCount).(int); ok {
-		return retryCount
-	}
-	return 1
 }
--- internal/models/node/node.go
+++ internal/models/node/node.go
@@ -1,10 +1,17 @@
 package node
 
 import (
+	"context"
+	"github.com/samanazadi/load-balancer/configs"
+	"github.com/samanazadi/load-balancer/pkg/logging"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"sync"
+	"time"
 )
+
+const RetryCount = iota
 
 // Node is a single backend server
 type Node struct {
@@ -26,11 +33,49 @@ func (n *Node) IsAlive() bool {
 	return n.alive
 }
 
-func New(url *url.URL, rp *httputil.ReverseProxy, alive bool) *Node {
+type LB interface {
+	ServeHTTP(http.ResponseWriter, *http.Request)
+	SetNodeAlive(*url.URL, bool)
+}
+
+func New(url *url.URL, alive bool, cfg *configs.Config, lb LB) *Node {
+	rp := httputil.NewSingleHostReverseProxy(url)
+	rp.ErrorHandler = newReverseProxyErrorHandler(cfg, lb, url, rp)
 	n := &Node{
 		URL:          url,
 		ReverseProxy: rp,
 	}
 	n.SetAlive(alive)
 	return n
+}
+
+func newReverseProxyErrorHandler(cfg *configs.Config, lb LB, url *url.URL, rp *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request, error) {
+	return func(rw http.ResponseWriter, r *http.Request, e error) { // Active health check
+		retries := getRetryCountFromContext(r)
+		logging.Logger.Printf("active health check, node down, %d retires: %s (%s)",
+			retries, url, e.Error())
+		if retries < cfg.HealthCheck.Active.MaxRetry {
+			// same node, more retries after some delay
+			retryDelay := time.Millisecond * time.Duration(cfg.HealthCheck.Active.RetryDelay)
+			select {
+			case <-time.After(retryDelay):
+				ctx := context.WithValue(r.Context(), RetryCount, retries+1)
+				rp.ServeHTTP(rw, r.WithContext(ctx))
+			}
+			return
+		}
+
+		// max retries exceeded
+		logging.Logger.Printf("active health check, node down, retires exceeded: %s", url)
+		lb.SetNodeAlive(url, false)
+		newCtx := context.WithValue(r.Context(), RetryCount, 1)
+		lb.ServeHTTP(rw, r.WithContext(newCtx))
+	}
+}
+
+func getRetryCountFromContext(r *http.Request) int {
+	if retryCount, ok := r.Context().Value(RetryCount).(int); ok {
+		return retryCount
+	}
+	return 1
 }
